b20865ac83fc63fa3cb93b6159a43be635485f7c
tealslug <tealslug@lionsoft.com>
2025-07-19 12:43:10 +0200

    Setup groups so that drawing and updating will be easier going forward.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 from constants import *
 from player import Player
 
-def game_loop(screen, player):
+def game_loop(screen, player, updatables, drawables):
     clock = pygame.time.Clock()
     dt = 0
 
@@ -12,19 +12,25 @@ def game_loop(screen, player):
                return
 
         screen.fill(pygame.Color('black'))
-        player.update(dt)
-        player.draw(screen)
-        pygame.display.flip()
 
+        updatables.update(dt)
+        for drawable in drawables:
+            drawable.draw(screen)
+
+        pygame.display.flip()
         dt = clock.tick(FPS) / 1000
 
 def main():
     print("Starting Asteroids!")
 
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    Player.containers = (updatables, drawables)
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    game_loop(screen, player)
+    game_loop(screen, player, updatables, drawables)
     pygame.quit()
 
 
